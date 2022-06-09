a8fa98712da7c7dde9c9cd0caad6151bec545c0c
unknown <sebasbeltran95@hotmail.com>
2022-06-09 10:31:33 -0500

    Examen final

---
+++ Financiera.AppWeb/Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Financiera.Logic;
+
+namespace Financiera.AppWeb.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var listarClientes = ClienteBL.Listar();
+            return View(listarClientes);
+        }
+    }
+}
